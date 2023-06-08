3d7842c753f19427c976c0d9c6cdfe8d7dcf95e5
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-08 01:34:32 +0000

    feat: add event listener to get user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,12 @@
     //define variables
     let list = $('#list');
     let button = $('#button');
-    let formInput = $('#input').val();
+    let formInput = '';
 
     //add event listener to get input 
+    $('#input').change(function () {
+        formInput = $('#input').val();
+    })
     
     //add list items when button is clicked
     button.click(function () {
