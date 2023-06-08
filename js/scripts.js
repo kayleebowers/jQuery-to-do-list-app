6d08d5afd6de97a29e4b7ad9220e8b8644ace39b
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-08 02:27:24 +0000

    style: format scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,26 @@
 (function ($) {
-    //define variables
-    let list = $('#list');
-    let button = $('#button');
-    let formInput = '';
+  //define variables
+  let list = $("#list");
+  let button = $("#button");
+  let formInput = "";
 
-    //add event listener to get input 
-    $('#input').change(function () {
-        formInput = $('#input').val();
-    })
+  //add event listener to get input
+  $("#input").change(function () {
+    formInput = $("#input").val();
+  });
 
-    //create cross out button
-    
+  //create cross out button
 
-    //add list items function
-    button.click(function () {
-        if (!formInput) {
-            alert("You must write something!");
-        } else {
-            let listItem = $('<li></li>').text(formInput).addClass('list-item');
-            list.append(listItem);
-            let crossOutButton = $('<crossOutButton></crossOutButton');
-            crossOutButton.append(document.createTextNode('X'));
-            listItem.append(crossOutButton);
-        }
-    })
-
-    //make delete buttons
-    // $('li').append(document.createTextNode('X'););
-
+  //add list items function
+  button.click(function () {
+    if (!formInput) {
+      alert("You must write something!");
+    } else {
+      let listItem = $("<li></li>").text(formInput).addClass("list-item");
+      list.append(listItem);
+      let crossOutButton = $("<crossOutButton></crossOutButton");
+      crossOutButton.append(document.createTextNode("X"));
+      listItem.append(crossOutButton);
+    }
+  });
 })(jQuery);
