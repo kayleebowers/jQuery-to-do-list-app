dcd79779e98ca2cda506f1b183d1e43c8cd0689f
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-08 02:26:38 +0000

    feat: add crossOutButton to all new li

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,21 @@
         formInput = $('#input').val();
     })
 
+    //create cross out button
+    
+
     //add list items function
     button.click(function () {
-        if (formInput = '') {
+        if (!formInput) {
             alert("You must write something!");
         } else {
             let listItem = $('<li></li>').text(formInput).addClass('list-item');
             list.append(listItem);
+            let crossOutButton = $('<crossOutButton></crossOutButton');
+            crossOutButton.append(document.createTextNode('X'));
+            listItem.append(crossOutButton);
         }
     })
-
-    //create cross out button
-    let crossOutButton = $('<crossOutButton></crossOutButton');
-    crossOutButton.append(document.createTextNode('X'));
-    li.append(crossOutButton);
 
     //make delete buttons
     // $('li').append(document.createTextNode('X'););
